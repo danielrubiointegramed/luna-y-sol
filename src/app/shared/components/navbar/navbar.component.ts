8c5878f8eab133bc69f0701b2b534255c869b706
danielrubiointegramed <tu-correo@example.com>
2025-02-25 09:28:54 -0600

    cambio de menú para dispositivos móviles y cambio de extensión de imágenes

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,28 +1,35 @@
 import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  standalone: true, // ✅ Necesario si es un componente independiente
-  imports: [RouterModule, CommonModule], // ✅ Asegura que RouterModule esté importado
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  showMobileMenu = false; // Estado del menú móvil
+
   constructor(private router: Router) {}
 
-  navigateTo(path: string, event: Event) {
+  // Alterna el menú móvil
+  toggleMenu(): void {
+    this.showMobileMenu = !this.showMobileMenu;
+  }
+
+  // Navega a la ruta y oculta el menú
+  navigateTo(path: string, event: Event): void {
     event.preventDefault();
-    console.log('Clic detectado en:', path);
-    this.router.navigateByUrl(path).then(success => {
-      if (success) {
-        console.log('Navegación exitosa a:', path);
-      } else {
-        console.error('Error en la navegación a:', path);
-      }
-    }).catch(error => {
-      console.error('Error en el enrutamiento:', error);
-    });
+    this.showMobileMenu = false; // Oculta el menú móvil
+    this.router.navigateByUrl(path)
+      .then(success => {
+        if (success) {
+          console.log('Navegación exitosa a:', path);
+        } else {
+          console.error('Error en la navegación a:', path);
+        }
+      })
+      .catch(error => {
+        console.error('Error en el enrutamiento:', error);
+      });
   }
 }
